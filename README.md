2d590aef38e1eb6ea6029134cc4172eeaf7d60fa
kaoru kakinuma <gptk25@gmail.com>
2020-10-07 18:30:52 +0900

    mod test code

--- README.md
+++ README.md
@@ -14,3 +14,37 @@ libcontroller.a
 $ ls /home/include
 controller.h
 ```
+
+
+## Usage
+
+```c
+#include "eight_button_controller.h"
+#include "controller.h"
+
+static const char * const KEYBOARD = "/dev/input/event2";
+
+int main( void )
+{
+    EightButtonConfig config = {
+        .a     = KEY_M,
+        .b     = KEY_K,
+        .x     = KEY_J,
+        .y     = KEY_I,
+        .right = KEY_S,
+        .left  = KEY_A,
+        .up    = KEY_W,
+        .down  = KEY_Z,
+    };
+
+    Controller *pController = __new__EightButtonController( KEYBOARD, &config );
+    pController->Start( pController );
+
+    // snip
+
+    pController->Stop( pController );
+    pController = __del__EightButtonController( pController );
+
+    return 0;
+}
+```
--- test/main.c
+++ test/main.c
@@ -1,15 +1,28 @@
 #include "eight_button_controller.h"
 #include "controller.h"
 
-#define PATH "/dev/input/event2"
+static const char * const KEYBOARD = "/dev/input/event2";
 
 int main( void )
 {
-    EightButtonConfig config;
-    Controller *pController;
-    pController = __new__EightButtonController( PATH, &config );
+    EightButtonConfig config = {
+        .a     = KEY_M,
+        .b     = KEY_K,
+        .x     = KEY_J,
+        .y     = KEY_I,
+        .right = KEY_S,
+        .left  = KEY_A,
+        .up    = KEY_W,
+        .down  = KEY_Z,
+    };
+
+    Controller *pController = __new__EightButtonController( KEYBOARD, &config );
     pController->Start( pController );
+
+    // snip
+
     pController->Stop( pController );
     pController = __del__EightButtonController( pController );
+
     return 0;
 }
